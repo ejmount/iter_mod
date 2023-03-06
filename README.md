ce3a7e116f185c2fcb016598d4a6894e72813971
Ewan Mount <ejmount@users.noreply.github.com>
2023-03-06 22:03:21 +0000

    Rewrite to use array of enums

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # iter_mod
 
-This crate provides an macro that allows for iterating over the constant and static members of a module, by both name and value. To demonstrate with a slightly modified example from [the reference](https://doc.rust-lang.org/reference/items/constant-items.html):
+This crate provides an macro that generates an array of the static and constant items in a given module. To demonstrate with a slightly modified [example from the reference](https://doc.rust-lang.org/reference/items/constant-items.html):
 
 ```rust
 #[iter_mod::make_iter(name="iter")]
@@ -23,15 +23,22 @@ mod example {
 }
 
 fn main() {
-    let numbers = example::iter::<u32>();
-    assert_eq!(numbers.count(), 2);
-    let mut structs = example::iter::<example::BitsNStrings>();
-    let (name, value) = structs.next().unwrap();
-    assert_eq!(name, "BITS_N_STRINGS");
-    assert_eq!(value.mybits, [1, 2]);
+    use example::ITEMS; 
+    use example::Item; 
+    assert_eq!(ITEMS.len(), 5);
+
+    let uints = ITEMS.iter().filter(|(_, b)| matches!(b, Item::U32(_))).count();
+    assert_eq!(uints, 2);
+
+    let uints = example::ITEMS.iter().filter(|(_, b)| matches!(b, Item::U32_2(_))).count();
+    assert_eq!(uints, 1);
+
+    let uints = example::ITEMS.iter().filter(|(_, b)| matches!(b, Item::BitsNStrings(_))).count();
+    assert_eq!(uints, 1);
+
+    assert_eq!("STRING", ITEMS[3].0);
+
+    let example::Item::StrRef(&s) = ITEMS[3].1 else { panic!() };
+    assert_eq!("bitstring", s);
 }
 ```
-
-When a module is tagged with the `make_iter` macro, a function will be generated within the module with the provided name. (The name is optional and defaults to `"iter"`) Because the members may be of heterogenous types, this function accepts a generic type parameter `T: 'static`, and returns an opaque `Iterator` of `(&'static str, &'static T)` pairs, one for each module member of type `T`. The provided type must be concrete, i.e. not a trait, and match the member exactly - no conversions via `From`, `Deref`, `Borrow`, etc are available.
-
-An additional caveat is that there is currently limited support for managing privacy - the generated function is marked `pub(super)`, and all matching members are returned, including any that may not otherwise be visible to the caller.
--- src/lib.rs
+++ src/lib.rs
@@ -1,100 +1,91 @@
+use std::collections::HashMap;
+
 use proc_macro::TokenStream;
 use quote::{format_ident, ToTokens};
-use structmeta::StructMeta;
-use syn::{parse_quote, Expr, Ident, Item, ItemMod, LitStr, Type};
+use syn::{parse_quote, Expr, Ident, Item, ItemMod, Type};
 
-#[derive(StructMeta)]
-struct Attributes {
-    name: Option<LitStr>,
+#[derive(Clone)]
+struct MetaType {
+    name: Ident,
+    typ: Type,
+    type_name: Ident,
+    expr: Expr,
 }
 
-fn append_iterator(items: &mut Vec<Item>, name: Ident) {
-    let mut item_names = vec![];
-    let mut item_types = vec![];
-    let mut item_exprs = vec![];
-    for item in items.iter() {
-        let ident = match item {
-            Item::Const(konst) => &konst.ident,
-            Item::Static(statik) => &statik.ident,
-            _ => {
-                continue;
-            }
-        };
-        let typ = match item {
-            Item::Const(konst) => &konst.ty,
-            Item::Static(statik) => &statik.ty,
-            _ => {
-                continue;
-            }
-        };
-        let type_ident = name_of_type(typ);
-        let expr: Expr = parse_quote!(&(#ident));
-        item_exprs.push(expr);
-        item_names.push(ident);
-        item_types.push(type_ident);
+/// Given a module, append the array of item references to it
+fn append_iterator(items: &mut Vec<Item>) {
+    let item_exprs: Vec<_> = items.iter().filter_map(get_metatype_for_item).collect();
+
+    let type_set: HashMap<_, _> = item_exprs
+        .iter()
+        .cloned()
+        .map(|mt| (mt.type_name, mt.typ))
+        .collect();
+
+    let filled_variants = type_set.into_iter().map(|(name, typ)| -> syn::Variant {
+        parse_quote! {
+            #name(&'static (#typ))
+        }
+    });
+
+    let filled_enum: syn::ItemEnum = parse_quote! {
+        #[non_exhaustive]
+        pub(super) enum Item {
+            #(#filled_variants,)*
+        }
+    };
+
+    let item_values = item_exprs.into_iter().map(create_item_reference);
+
+    let list: syn::ItemStatic = parse_quote! {
+        pub(super) static ITEMS: &[(&'static str, Item)] = &[#(#item_values,)*];
+    };
+
+    items.push(Item::Static(list));
+    items.push(Item::Enum(filled_enum));
+}
+
+fn create_item_reference(mt: MetaType) -> syn::Expr {
+    let MetaType {
+        name,
+        expr,
+        type_name,
+        ..
+    } = mt;
+    let name = name.to_string();
+    parse_quote! {
+        (#name, Item::#type_name(#expr))
     }
-    let count = item_names.len();
-    let name_strs: Vec<_> = item_names.into_iter().map(syn::Ident::to_string).collect();
-
-    let strukt = parse_quote! {
-        pub(super) struct ModIterator<T:'static> {
-            NAMES: [&'static str; #count],
-            VALS: [&'static dyn core::any::Any; #count],
-            _phantom: std::marker::PhantomData<T>,
-            index: usize,
-        }
-    };
-
-    let imp = parse_quote! {
-        impl<T: 'static> ModIterator<T> {
-            const fn new() -> ModIterator<T> {
-                ModIterator {
-                    NAMES: [#(#name_strs,)*],
-                    VALS: [#(#item_exprs,)*],
-                    _phantom: std::marker::PhantomData,
-                    index: 0,
-                }
-            }
-            pub(super) const  fn next(&mut self) -> Option<(&'static str, &'static T)> {
-                while self.index < #count {
-                    let val: &'static T = unsafe { std::mem::transmute(self.VALS[self.index])  };
-                    let item = (self.NAMES[self.index], val);
-                    self.index += 1;
-                    return Some(item);
-
-                }
-                //else {
-                    return None;
-                //}
-            }
-        }
-    };
-
-    let iterator_impl = parse_quote! {
-        impl<T> Iterator for ModIterator<T> {
-            type Item =  (&'static str, &'static T);
-
-            fn next(&mut self) -> Option<(&'static str, &'static T)> {
-                self.next()
-            }
-        }
-    };
-
-    let fn_body = parse_quote! {
-        pub(super) const fn #name <T:'static>() -> ModIterator<T> {
-            ModIterator::new()
-        }
-    };
-
-    items.push(Item::Struct(strukt));
-    items.push(Item::Impl(imp));
-    items.push(Item::Impl(iterator_impl));
-    items.push(Item::Fn(fn_body));
 }
 
+fn get_metatype_for_item(expr: &Item) -> Option<MetaType> {
+    let (name, typ) = match expr {
+        Item::Const(expr) => (&expr.ident, &expr.ty),
+        Item::Static(expr) => (&expr.ident, &expr.ty),
+        _ => return None,
+    };
+    let type_name = name_of_type(typ);
+    let expr: Expr = parse_quote!(&(#name));
+    MetaType {
+        name: name.clone(),
+        typ: *typ.clone(),
+        type_name,
+        expr,
+    }
+    .into()
+}
+
+/// Given a type, constructs a name suitable for an enum variant representing it.
+/// For ADTs, this is called recursively to build up, e.g. the name of a tuple from component names
+/// Uppercases the return value to avoid warnings from rustfmt
+/// TODO: Improve that
 fn name_of_type(typ: &Type) -> Ident {
-    match typ {
-        Type::Array(typ) => name_of_type(&typ.elem),
+    let name = match typ {
+        Type::Array(typ) => {
+            let count = typ.len.to_token_stream().to_string();
+            // Not great, liable to break if something complicated is used for the length
+            format_ident!("{}_{}", name_of_type(&typ.elem), count)
+        }
         Type::Path(typ) => typ
             .path
             .segments
@@ -102,51 +93,50 @@ fn name_of_type(typ: &Type) -> Ident {
             .expect("Got empty path??")
             .ident
             .clone(),
+        // Get the last segment of the path as the name
+        // Might cause problems if multiple types in scope have the same name
+        // TODO: Also doesnt deal with generics at all
         Type::Ptr(typ) => {
             let inner_name = name_of_type(&typ.elem);
             let mutt = if typ.mutability.is_some() { "Mut" } else { "" };
             format_ident!("{inner_name}{mutt}Ptr")
         }
-        Type::Never(_) => format_ident!("Never"),
+        Type::Never(_) => format_ident!("Never"), // ! also isn't a valid variant name
         Type::Tuple(typ) => {
-            let names: Vec<_> = typ
-                .elems
-                .iter()
-                .map(name_of_type)
-                .map(|i| i.to_string())
-                .collect();
-            format_ident!("{}", names.join(""))
+            if typ.elems.is_empty() {
+                format_ident!("Unit") // () is not a valid identifier for enum variants
+            } else {
+                let names: Vec<_> = typ
+                    .elems
+                    .iter()
+                    .map(name_of_type)
+                    .map(|i| i.to_string())
+                    .collect();
+                format_ident!("{}", names.join(""))
+            }
         }
-        _ => panic!("Unsupported type {:?}", typ.to_token_stream()),
-    }
+        Type::Reference(r) => format_ident!("{}Ref", name_of_type(&r.elem)),
+        _ => panic!("Unsupported type {:?}", typ.to_token_stream(),),
+    };
+    let mut name_str = name.to_string();
+    let (begin, _) = name_str.split_at_mut(1);
+    begin.make_ascii_uppercase();
+    Ident::new(&name_str, name.span())
 }
 
+/// This attribute generates two additional items in the module it is applied to:
+/// * An enum called `Item`, which has a variant for each unique type among the constant and static items in the module. It is marked `[non_exhaustive]` so that adding items in the future is not breaking.
+/// * An array called `ITEMS`, consisting of pairs of a `&'static str` denoting the name of the item, and a `Item` instance containing a reference to the value. The values are in source order.
 ///
-/// This macro generates a function inside the module it's applied to, with the provided name. (Defaulting to "iter")
-/// The function returns an Iterator of name-reference tuples for each applicable const or static value in the module.
-/// The module's values may be heterogenous, so the function takes a generic `T`, and returns only the values of the given type.
+/// Both of these new items have a visibility of `pub(super)`.
 ///
-///
-///
-/// generic type parameter `T`, and returns an opaque `Iterator` of `(&'static str, &'static T)` pairs, one for each
-/// module member of type `T`. The provided type must be concrete, i.e. not a trait, and match the member exactly - no
-/// conversions via `From`, `Deref`, `Borrow`, etc are available.
-/// An additional caveat is that there is currently limited support for managing privacy - the generated function is marked
-/// `pub(super)`, and all members of the given type are returned, including any that may not otherwise be visible to the caller.
-///
-///
-
+/// This currently has several caveats:
+/// * Not all possible types are supported - if you have a usecase that's not supported, please file a bug
+/// * Faulty output may result from the type's base name being the same - use type aliases to distinguish the type names as seen by the macro. This can occur when:
+/// ** Types differ only in generic parameters
+/// ** Multiple types with the same base name are imported from different modules
 #[proc_macro_attribute]
-pub fn make_iter(attr: TokenStream, module: TokenStream) -> TokenStream {
-    let args: Attributes = syn::parse(attr).unwrap();
-
-    let name = args
-        .name
-        .as_ref()
-        .map(LitStr::value)
-        .unwrap_or("iter".to_owned());
-    let name = format_ident!("{name}");
-
+pub fn make_iter(_attr: TokenStream, module: TokenStream) -> TokenStream {
     let mut output = TokenStream::new().into();
 
     if let Ok(
@@ -156,7 +146,7 @@ pub fn make_iter(attr: TokenStream, module: TokenStream) -> TokenStream {
         },
     ) = &mut syn::parse(module)
     {
-        append_iterator(&mut m.content.as_mut().unwrap().1, name);
+        append_iterator(&mut m.content.as_mut().unwrap().1);
         m.to_tokens(&mut output);
     } else {
         panic!("Could not parse item as module");
